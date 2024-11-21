fc0d9541b429dd8efddfed01d66941777328bd2f
unknown <zoramexmiki@gmail.com>
2024-11-21 12:00:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep z Zegarkami</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/66e4cac288.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="main d-flex">
+    <?php require  'nav.php';?>
+    </div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,40 @@
+<div class="nav w-100 d-flex justify-content-center f-direction-col">
+   <div class="navbar d-flex  w-80 ">
+        <div class="navbar-search col-sm-3 d-flex">
+        <a class="navbar-link" href="#">
+        <i class="fa fa-search "></i>
+        </a>    
+        </div>
+        <div class="navbar-logo col-sm-3 d-flex justify-content-center">
+            <img src="./img/image2vector.svg" alt="">
+        </div>
+        <div class="navbar-store d-flex col-sm-3">
+            <a class="navbar-link" href="#">
+            <i class="fa fa-bag-shopping"></i>
+            </a>
+        <a class="navbar-link small-font border-left" href="#">log in</a>
+        </div>    
+   </div>
+   <div class="navbar-links d-flex w-60 justify-content-center">
+        <ul class="navbar-list ">
+            <li class="navbar-item">
+                <a class="navbar-link active"  href="#">Home </a> 
+            </li>
+            <li class="navbar-item">
+                <a class="navbar-link"  href="#">Women's</a> 
+            </li>
+            <li class="navbar-item">
+                <a class="navbar-link"  href="#">Men's</a> 
+            </li>
+            <li class="navbar-item">
+                <a class="navbar-link"  href="#"></a>
+             </li>
+             <li class="navbar-item">
+                <a class="navbar-link"  href="#">home</a> 
+            </li>
+            <li class="navbar-item">
+                <a class="navbar-link"  href="#">Contact</a> 
+            </li>
+        </ul>
+   </div>
+</div>
